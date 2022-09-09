41840909c4940541d745b6abeef4290ac50db945
Vean <50613879+Veanyxz@users.noreply.github.com>
2022-09-09 21:18:33 +0700

    Update mongo.js

--- db/mongo.js
+++ db/mongo.js
@@ -1,7 +1,7 @@
 const monk = require('monk')
 
 // Connection URL
-var url = "mongodb+srv://caliph71:clph1122@cluster0.e1ccz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+var url = "mongodb+srv://vean:123@cluster0.vv8aleu.mongodb.net/?retryWrites=true&w=majority";
 try {
     if (url == '') throw console.log('Cek konfigurasi database, var url belum diisi');
 } catch (e) {
