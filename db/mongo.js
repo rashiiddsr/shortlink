a7199299cfab68227e49378e2d9514011b4d33d4
Rashid <74960869+rashiiddsr@users.noreply.github.com>
2023-04-11 22:58:25 +0700

    Update mongo.js

--- db/mongo.js
+++ db/mongo.js
@@ -1,7 +1,7 @@
 const monk = require('monk')
 
 // Connection URL
-var url = "mongodb+srv://vean:123@cluster0.vv8aleu.mongodb.net/?retryWrites=true&w=majority";
+var url = "mongodb+srv://rashiiddsr:GUI.2xc8:rashiiddsr.wckplv0.mongodb.net/?retryWrites=true&w=majority";
 try {
     if (url == '') throw console.log('Cek konfigurasi database, var url belum diisi');
 } catch (e) {
